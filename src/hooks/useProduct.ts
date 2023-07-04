c370db687b672510a9a49830acba9499e2f510b6
유영빈 <97148877+BTSBRINGMEHERE@users.noreply.github.com>
2023-07-04 15:33:39 +0900

    update settings
    리덕스, 엑시오스, 라우터, sass 등등 기본 설정

---
+++ src/hooks/useProduct.ts
@@ -0,0 +1,43 @@
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { useMemo } from "react";
+import axios from "axios";
+
+import { Product } from "../types/index";
+
+const create = axios.create();
+const API_URL = "https://fakestoreapi.com/products" as string;
+
+export const fetchProductAPI = async () => {
+  const BASE_URL = API_URL;
+  const { data } = await create.get<Product>(BASE_URL);
+  return data;
+};
+
+export const fetchProduct = createAsyncThunk("product", async () => {
+  const response = await fetchProductAPI();
+  return response;
+});
+
+interface ProductState {
+  products: Product;
+}
+
+const initialState = {
+  products: [],
+} as ProductState;
+
+export const productSlice = createSlice({
+  name: "products",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(
+      fetchProduct.fulfilled,
+      (state, action: PayloadAction<Product>) => {
+        state.products = [...action.payload];
+      }
+    );
+  },
+});
+
+export const productReducer = productSlice.reducer;
---
+++ src/types/index.ts
@@ -0,0 +1,12 @@
+export type Product = {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: "men's clothing" | "women's clothing" | "jewelery" | "electronics";
+  image: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
+}[];
